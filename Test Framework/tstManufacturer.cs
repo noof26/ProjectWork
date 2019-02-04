b8f481d023ccf8942e3cef13f1c89231a7847f3e
p16225481 <p16225481@my365.dmu.ac.uk>
2019-02-04 11:18:46 +0000

    Creation of clsManufacturer + test class

---
+++ Test Framework/tstManufacturer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Class_Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstManufacturer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // create an instance of our class clsManufacturer
+            clsManufacturer AnManufacturer = new clsManufacturer();
+            // check to see that the class is not null
+            Assert.IsNotNull(AnManufacturer);
+        }
+    }
+}
